853a3c3bdff54634aee25a999d085d77a9b414ce
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-11-05 13:31:24 +0100

    Design skeleton complete

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,16 +5,67 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calculator.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+    val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val sumDisplay = binding.sumDisplayTV
+        var calculateDisplay = binding.calculateDisplayTV
+
+        val digitsButtons = listOf(
+            binding.zeroButton,
+            binding.oneButton,
+            binding.twoButton,
+            binding.threeButton,
+            binding.fourButton,
+            binding.fiveButton,
+            binding.sixButton,
+            binding.sevenButton,
+            binding.eightButton,
+            binding.nineButton
+        )
+
+        val operationsButtons = listOf(
+            binding.plusButton,
+            binding.minusButton,
+            binding.multiplyButton,
+            binding.divideButton,
+            binding.powerOfButton,
+            binding.squareRootButton,
+            binding.clearButton,
+            binding.dotButton
+        )
+
+        for (digit in digitsButtons) {
+            digit.setOnClickListener {
+                val calculateDisplayText = calculateDisplay.text.toString() + digit.text
+                calculateDisplay.text = calculateDisplayText
+            }
+        }
+
+        for(operator in operationsButtons) {
+            operator.setOnClickListener{
+                val sumText = calculateDisplay.text.toString()+operator.text
+                sumDisplay.text = sumText
+                calculateDisplay.text =""
+
+            }
+        }
+
     }
+
+
 }
